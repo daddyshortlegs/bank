564937b75a4cc6d376deb3e597d792cad2e61640
andys <andysmith836@gmail.com>
2020-08-10 21:57:02 +0100

    Clean up acceptance test and add docstring

--- src/test/java/cucumber/Stepdefs.java
+++ src/test/java/cucumber/Stepdefs.java
@@ -1,24 +1,30 @@
 package cucumber;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Stepdefs {
 
-    @Given("a client makes a deposit of {int} on {int}-{int}-{int}")
-    public void a_client_makes_a_deposit_of_on(Integer int1, Integer int2, Integer int3, Integer int4) {
+    @Given("a client makes a deposit of {int} on {string}")
+    public void a_client_makes_a_deposit_of_on(Integer amount, String date) {
     }
 
-    @Given("a deposit of {int} on {int}-{int}-{int}")
-    public void a_deposit_of_on(Integer int1, Integer int2, Integer int3, Integer int4) {
+    @Given("a deposit of {int} on {string}")
+    public void a_deposit_of_on(Integer amount, String date) {
     }
 
-    @Given("a withdrawal of {int} on {int}-{int}-{int}")
-    public void a_withdrawal_of_on(Integer int1, Integer int2, Integer int3, Integer int4) {
+    @Given("a withdrawal of {int} on {string}")
+    public void a_withdrawal_of_on(Integer amount, String date) {
     }
 
     @When("they print their bank statement")
     public void they_print_their_bank_statement() {
+    }
+
+    @Then("they would see")
+    public void they_would_see(String statement) {
+
     }
 
 }
--- src/test/resources/cucumber/bank_account.feature
+++ src/test/resources/cucumber/bank_account.feature
@@ -2,12 +2,14 @@ Feature: Simple bank account
 
   Scenario: Print a statement for some transactions
 
-    Given a client makes a deposit of 1000 on 10-01-2012
-    And a deposit of 2000 on 13-01-2012
-    And a withdrawal of 500 on 14-01-2012
+    Given a client makes a deposit of 1000 on "10-01-2012"
+    And a deposit of 2000 on "13-01-2012"
+    And a withdrawal of 500 on "14-01-2012"
     When they print their bank statement
-#    Then they would see
-#      Date       | Amount | Balance
-#      14/01/2012 | -500   | 2500
-#      13/01/2012 | 2000   | 3000
-#      10/01/2012 | 1000   | 1000
+    Then they would see
+      """
+      Date       | Amount | Balance
+      14/01/2012 | -500   | 2500
+      13/01/2012 | 2000   | 3000
+      10/01/2012 | 1000   | 1000
+      """
